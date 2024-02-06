f56d2a7a16f638a6c61cdb727c8144748b3cd971
Ciuca Veronica <veronicaciuca17@gmail.com>
2024-02-06 20:38:58 +0200

    Modify the main.js file

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const images = document.querySelectorAll(".js-plus-btn")
 const answers = document.querySelectorAll(".answer")
 const titles = document.querySelectorAll(".title")
-console.log(titles)
-const minusBtn = document.querySelector(".js-minus-btn")
-const details = document.querySelector(".js-details")
 const firstTitle = document.querySelector(".first--title")
 let isVisible = true
 images.forEach((image, index) => {
@@ -18,18 +15,17 @@ images.forEach((image, index) => {
   })
 })
 
-minusBtn.addEventListener("click", () => {
-  details.classList.toggle("answer")
-})
-
 let index = 0
-document.body.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowDown") {
-    index = (index + 1) % titles.length
-    titles[index].focus({preventScroll: true})
-    answers[index].classList.add("visible")
-  }
-  if (e.key === "ArrowUp") {
-    answers[index].classList.remove("visible")
-  }
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowDown") {
+      index = (index + 1) % titles.length
+      titles[index].focus({preventScroll: true})
+      answers[index].classList.add("visible")
+    }
+    if (e.key === "ArrowUp") {
+      answers[index].classList.remove("visible")
+    }
+  })
 })
